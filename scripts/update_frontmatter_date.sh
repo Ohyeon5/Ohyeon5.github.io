72d853324b421ec9d822014c667198b209ab339f
ohhyeon <ohhyeon@choung.ch>
2026-05-01 22:40:55 +0200

    update date script to consider the draft

--- scripts/update_frontmatter_date.sh
+++ scripts/update_frontmatter_date.sh
@@ -2,5 +2,10 @@
 set -e
 today=$(date +%Y-%m-%d)
 for f in "$@"; do
-    sed -i "s/^modified: .*/modified: $today/" "$f"
+    if grep -qP "^draft:\s*true" "$f"; then
+        sed -i "s/^modified: .*/modified: $today/" "$f"
+        sed -i "s/^created: .*/created: $today/" "$f"
+    else
+        sed -i "s/^modified: .*/modified: $today/" "$f"
+    fi
 done
